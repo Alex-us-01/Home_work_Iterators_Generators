7f1d3c9befa440541d669bfb38b10b5026d4358e
Uskov_Aleksandr <Alex_us_01@mail.ru>
2024-04-22 00:12:01 +0300

    intermediate commit: iterating through the list

--- task_1.py
+++ task_1.py
@@ -8,33 +8,55 @@ class FlatIterator:
 
     def __init__(self, list_of_list):
         self.input_list = list_of_list
+        self.result = []
 
     def __iter__(self):
-        ...
+        self.counter = 0
+
         return self
 
     def __next__(self):
-        ...
-        return item
+        if self.counter < len(self.input_list):
+            item = self.input_list[self.counter]
+            self.counter += 1
+            self.result.extend(item)
+            # print(self.result)
+            return item
+        else:
+            raise StopIteration
 
 
-def test_1():
-
-    list_of_lists_1 = [
+list_of_lists_1 = [
         ['a', 'b', 'c'],
         ['d', 'e', 'f', 'h', False],
         [1, 2, None]
     ]
 
-    for flat_iterator_item, check_item in zip(
-            FlatIterator(list_of_lists_1),
-            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-    ):
+for i in FlatIterator(list_of_lists_1):
+    print(i)
 
-        assert flat_iterator_item == check_item
-
-    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+#['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
 
-if __name__ == '__main__':
-    test_1()
+
+
+# def test_1():
+#
+#     list_of_lists_1 = [
+#         ['a', 'b', 'c'],
+#         ['d', 'e', 'f', 'h', False],
+#         [1, 2, None]
+#     ]
+#
+#     for flat_iterator_item, check_item in zip(
+#             FlatIterator(list_of_lists_1),
+#             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+#     ):
+#
+#         assert flat_iterator_item == check_item
+#
+#     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+#
+#
+# if __name__ == '__main__':
+#     test_1()
